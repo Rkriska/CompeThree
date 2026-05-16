e568ab73544c8a22f8f83e0d9ea2fa0c75f0130e
akbarwijayaa <jimsaber02@gmail.com>
2026-05-16 14:57:00 +0700

    feat(joint-ventures): support multi-token deposits with USD pricing and whitelist

--- apps/contracts/src/JointVentures.sol
+++ apps/contracts/src/JointVentures.sol
@@ -1,25 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {IERC20} from "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IToken} from "./interfaces/IToken.sol";
 import {AccessControl} from "@openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {Pausable} from "@openzeppelin-contracts/contracts/utils/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {IPriceFeed} from "./interfaces/IPriceFeed.sol";
 
 contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
     
     bytes32 public constant FINANCE_ROLE = keccak256("FINANCE_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
+    IToken public tokenShares;
+    IPriceFeed public priceFeed;
+    
     address public finance;
     uint256 public target;
-    uint256 public collected;
+    mapping(address => uint256) public collected;
+    uint256 public collectedUSD;
     bool public active;
 
-    string public name;
+    mapping(address => bool) public isTokenInWhitelist;
+
+    string public nameMarket;
     address[] public registeredMembers;
 
-    mapping(address => uint256) public contribution;
+    mapping(address => mapping(address => uint256)) public contributions;
 
     struct MemberData {
         string name;
@@ -33,6 +41,7 @@ contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
     error RegisteredMember();
     error NotActive();
     error NotRegisteredMember();
+    error NotWhitelistedToken();
 
     event FinanceSet(address newFinance);
     event Activate(bool activate);
@@ -41,8 +50,13 @@ contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
     event Deposited(address indexed member, uint256 amount);
 
     constructor(
+        address _tokenShares,
+        address _priceFeed
     ) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        tokenShares = IToken(_tokenShares);
+        priceFeed = IPriceFeed(_priceFeed);
     }
 
     function setFinance(address _finance) external onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -54,6 +68,10 @@ contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
         active = _activate;
 
         emit Activate(_activate);
+    }
+
+    function setTokenWhitelist(address _token) external onlyRole(FINANCE_ROLE) {
+        isTokenInWhitelist[_token] = true;
     }
 
     function setTarget(uint256 _target) external onlyRole(FINANCE_ROLE) {
@@ -71,11 +89,12 @@ contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
         emit Registered(msg.sender, _name);
     }
 
-    function deposit(address _token, uint256 _amount) external nonReentrant whenNotPaused {
+    function deposit(address _tokenIn, uint256 _amount) external nonReentrant whenNotPaused {
         require(active, NotActive());
-        require(members[msg.sender].isMember, NotRegisteredMember());
-
-        _deposit(_token, _amount);
+        if (members[msg.sender].isMember == false) revert NotRegisteredMember();
+        if (!isTokenInWhitelist[_tokenIn]) revert NotWhitelistedToken();
+        
+        _deposit(_tokenIn, _amount);
 
         emit Deposited(msg.sender, _amount);
     }
@@ -89,10 +108,15 @@ contract JointVentures is AccessControl, Pausable, ReentrancyGuard {
     }
 
     function _deposit(address _token, uint256 _amount) internal {
-        contribution[msg.sender] += _amount;
-        collected += _amount;
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
 
+        contributions[msg.sender][_token] += _amount;
+        collected[_token] += _amount;
 
-        IToken(_token).mintByContract(msg.sender, _amount);
+        (uint256 price, uint256 decimals) = priceFeed.getChainlinkDataFeedLatestAnswer(_token);
+        uint256 amountInUSD = (_amount * price) / (10 ** decimals);
+        collectedUSD += amountInUSD;
+
+        tokenShares.mintByContract(msg.sender, _amount);
     }
 }
--- apps/contracts/test/JointVentures.t.sol
+++ apps/contracts/test/JointVentures.t.sol
@@ -4,86 +4,83 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {JointVentures} from "../src/JointVentures.sol";
 import {Token} from "../src/Token.sol";
+import {PriceFeeds} from "../src/PriceFeed.sol";
 import {IERC20} from "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract JointVenturesTest is Test {
     JointVentures public jointVentures;
     Token public token;
+    PriceFeeds public priceFeed;
+
+    address constant USDC = 0xcebA9300f2b948710d2653dD7B07f33A8B32118C;
+    address constant USDT = 0x617f3112bf5397D0467D315cC709EF968D9ba546;
 
     address public owner = address(0x1234);
     address public finance = address(0x123);
     address public member1 = address(0x1);
 
     function setUp() public {
+        vm.createSelectFork("celo");
+
         vm.startPrank(owner);
-        jointVentures = new JointVentures();
-        token = new Token(address(this), address(jointVentures));
+        token = new Token(address(owner));
+        priceFeed = new PriceFeeds(USDC, USDT);
+        jointVentures = new JointVentures(address(token), address(priceFeed));
+        token.setJointVenture(address(jointVentures));
 
         // Granting role
         jointVentures.grantRole(jointVentures.FINANCE_ROLE(), finance);
         jointVentures.grantRole(jointVentures.OPERATOR_ROLE(), finance);
         vm.stopPrank();
-    }
 
-
-    function testDeposit() public {
-        vm.prank(owner);
-        jointVentures.setFinance(finance);
-        
-        vm.prank(finance);
+        vm.startPrank(finance);
         jointVentures.activate(true);
+        // Whitelist tokens
+        jointVentures.setTokenWhitelist(USDC);
+        jointVentures.setTokenWhitelist(USDT);
+        vm.stopPrank();
 
         vm.prank(member1);
         jointVentures.register("test");
+    }
+
+    function test_depositUseUSDC() public {
+        uint256 amount = 100 * 1e6; // 100 USDC
+        deal(USDC, member1, amount);
 
         vm.startPrank(member1);
-        jointVentures.deposit(address(token), 100);
-        uint256 amount = jointVentures.contribution(member1);
+        IERC20(USDC).approve(address(jointVentures), amount);
+        jointVentures.deposit(USDC, amount);
         vm.stopPrank();
 
-        console.log("Member contribution:", amount);
+        uint256 contribution = jointVentures.contributions(member1, USDC);
+        uint256 collectedUSD = jointVentures.collectedUSD();
+        
+        console.log("Member contribution:", contribution);
         console.log("Member token balance:", token.balanceOf(member1));
+        console.log("Collected USD:", collectedUSD);
 
-        assertEq(amount, 100, "Member contribution should be 100");
+        assertEq(contribution, amount, "Member contribution should be 100 USDC");
         assertEq(token.balanceOf(member1), amount, "Token balance should be 100");
     }
 
-    function test_setFinance() public {
-        address newFinance = address(0x456);
-        vm.prank(owner);
-        jointVentures.setFinance(newFinance);
-    }
+    function test_depositUseUSDT() public {
+        uint256 amount = 100 * 1e6; // 100 USDT
+        deal(USDT, member1, amount);
 
+        vm.startPrank(member1);
+        IERC20(USDT).approve(address(jointVentures), amount);
+        jointVentures.deposit(USDT, amount);
+        vm.stopPrank();
 
-    function test_depositWhenNotPaused() public {
-        vm.prank(owner);
-        jointVentures.setFinance(finance);
-        
-        vm.prank(finance);
-        jointVentures.activate(true);
+        uint256 contribution = jointVentures.contributions(member1, USDT);
+        uint256 collectedUSD = jointVentures.collectedUSD();
 
-        vm.prank(member1);
-        jointVentures.register("test");
+        console.log("Member contribution:", contribution);
+        console.log("Member token balance:", token.balanceOf(member1));
+        console.log("Collected USD:", collectedUSD);
 
-        vm.prank(member1);
-        jointVentures.deposit(address(token), 100);
-    }
-
-    function test_depositWhenPaused() public {
-        vm.prank(owner);
-        jointVentures.setFinance(finance);
-        
-        vm.prank(finance);
-        jointVentures.activate(true);
-
-        vm.prank(member1);
-        jointVentures.register("test");
-
-        vm.prank(owner);
-        jointVentures.pause();
-
-        vm.prank(member1);
-        vm.expectRevert();
-        jointVentures.deposit(address(token), 100);
+        assertEq(contribution, amount, "Member contribution should be 100 USDT");
+        assertEq(token.balanceOf(member1), amount, "Token balance should be 100");
     }
 }
